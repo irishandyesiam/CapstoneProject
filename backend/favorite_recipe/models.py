6f641b90dc56ec47fc2cb5b5f053b4f87189de89
irishandyesiam <nathan.irish@gmail.com>
2022-10-17 09:53:10 -0500

    Nuked mySQL database and re-migrated tables.

--- backend/favorite_recipe/models.py
+++ backend/favorite_recipe/models.py
@@ -1,10 +1,10 @@
 
 from django.db import models;
-from user.models import User;
+from authentication.models import User;
 
 # Create your models here.
 class Favorite(models.Model):
-    user_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     recipe = models.CharField(max_length=255)
     rating = models.IntegerField()
     comments = models.TextField()
--- backend/meal_planner/models.py
+++ backend/meal_planner/models.py
@@ -1,9 +1,9 @@
-
 from django.db import models
-from user.models import User
+from authentication.models import User
 from favorite_recipe.models import Favorite
 
 # Create your models here.
 class MealPlan(models.Model):
     recipe = models.ForeignKey(Favorite, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    day_week = models.CharField(max_length = 255)
