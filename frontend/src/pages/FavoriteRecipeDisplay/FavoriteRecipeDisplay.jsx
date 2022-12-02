09cca53a73da4633ca1a5f625dca60c409900f37
irishandyesiam <nathan.irish@gmail.com>
2022-12-01 22:59:23 -0600

    Props passed to FavoriteRecipeDisplay.

--- frontend/src/pages/FavoriteRecipeDisplay/FavoriteRecipeDisplay.jsx
+++ frontend/src/pages/FavoriteRecipeDisplay/FavoriteRecipeDisplay.jsx
@@ -4,8 +4,12 @@ import useAuth from "../../hooks/useAuth";
 import axios from "axios";
 
 const FavoriteRecipeDisplay = (favorite_recipe) => {
-    console.log(favorite_recipe)
-   
-};
+    console.log(favorite_recipe.favorite_recipe.recipe.name);
+    return (
+      <div>
+        <h1>{favorite_recipe.favorite_recipe.recipe.name}</h1>
+       </div>
+    );
+}
 
 export default FavoriteRecipeDisplay
