1371fca509d734cb49fb7ed53b30d26a1cc02c14
irishandyesiam <nathan.irish@gmail.com>
2022-12-01 23:13:34 -0600

    Image displayed on FavoriteRecipeDisplay.

--- frontend/src/pages/FavoriteRecipeDisplay/FavoriteRecipeDisplay.jsx
+++ frontend/src/pages/FavoriteRecipeDisplay/FavoriteRecipeDisplay.jsx
@@ -4,9 +4,14 @@ import useAuth from "../../hooks/useAuth";
 import axios from "axios";
 
 const FavoriteRecipeDisplay = (favorite_recipe) => {
-    console.log(favorite_recipe.favorite_recipe.recipe.name);
+    console.log(favorite_recipe.favorite_recipe.recipe.image);
     return (
       <div>
+        <img
+                className="img"
+                src={favorite_recipe.favorite_recipe.recipe.image}
+                alt={"unavailable"}
+              /> 
         <h1>{favorite_recipe.favorite_recipe.recipe.name}</h1>
        </div>
     );
