bfbccfe2e73bbcb0fb7daa5312e6627e55c73ea8
irishandyesiam <nathan.irish@gmail.com>
2022-12-15 23:42:35 -0600

    Urls pathway included with backend urls. GET and POST requests written in views.py. Will need to test urls with POSTMAN.

--- backend/favorite_recipe/urls.py
+++ backend/favorite_recipe/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# When comment GET and POST are working, remove path 'comment'/
+
 urlpatterns = [
     path('', views.favorites_list),
     path('comment/', views.favorites_comment),
