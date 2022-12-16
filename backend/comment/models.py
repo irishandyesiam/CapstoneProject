261d831a3831cd2bfa37edc4ef312049f1d97e39
irishandyesiam <nathan.irish@gmail.com>
2022-12-15 23:28:03 -0600

    Created comment app + table. Migrated to DB.

--- backend/comment/models.py
+++ backend/comment/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from authentication.models import User;
+from recipes.models import Recipe;
 
 # Create your models here.
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, null=True)
+    text = models.CharField(max_length=2000)
