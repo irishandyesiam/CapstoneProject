8f754b54df8ca5f4d6ab84437bd2ae453151b292
irishandyesiam <nathan.irish@gmail.com>
2023-01-30 10:49:05 -0600

    Pending changes update.

--- frontend/src/components/DisplayRecipeBook/DisplayRecipeBook.jsx
+++ frontend/src/components/DisplayRecipeBook/DisplayRecipeBook.jsx
@@ -29,7 +29,7 @@ return (
         userRecipes.map((recipes) => (
           <p>
             {recipes.name} 
-            <RecipeDetails />
+            <RecipeDetails recipes={userRecipes}/>
           </p>
           
           
--- frontend/src/components/RecipeDetails/RecipeDetails.jsx
+++ frontend/src/components/RecipeDetails/RecipeDetails.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import { useState } from 'react';
 
-const RecipeDetails = () => {
+const RecipeDetails = (props) => {
+    console.log("Is anything getting passed", props.recipes)
     const [toggle, setToggle] = useState(true);
+    const [recipeDetails, setRecipeDetails] = useState;
 
 
 const handleClick = (event) => {
     setToggle(!toggle);
     console.log("OnClick event data", event)
+    setRecipeDetails();
 };
 
 return (
     <div>
-            <button value={toggle}
-            onClick={(event) => handleClick(event.target.value)} 
+            <button value={recipeDetails}
+            onClick={(event) => handleClick(setRecipeDetails(event.target.value))} 
             class="btn btn-info mb-5">
             {toggle ? "Less" : "More"}
             </button>
 
             {toggle ?
                 <ul class="list-group">
-                <li class="list-group-item">Value One</li>
+                <li class="list-group-item">{props.recipeDetails}</li>
                 <li class="list-group-item">Value Two</li>
                 </ul>
                 :
