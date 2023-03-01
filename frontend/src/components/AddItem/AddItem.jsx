6ffd57e4f552848ea630b0965f7df391958cbd50
irishandyesiam <nathan.irish@gmail.com>
2023-02-28 23:03:18 -0600

    Removed unassigned state variables in AddItem component. Created new component Comment to get user comments from DB to display.

--- frontend/src/components/AddItem/AddItem.jsx
+++ frontend/src/components/AddItem/AddItem.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { useContext, useState } from "react";
-import AuthContext from "../../context/AuthContext";
+import { useState } from "react";
 
 
 
---
+++ frontend/src/components/Comments/Comments.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {useState} from 'react'; 
+import useAuth from '../../hooks/useAuth';
+import axios from 'axios';
+
+const GetComment = () => {
+    const [user, token] = useAuth();
+    const [comments, setComments] = useState([]);
+
+useEffect (() => {
+    const fetchComments = async () => {
+        try {
+            let response = await axios.get("http://127.0.0.1:8000/api/comment/", {
+            headers: {
+                Authorization: "Bearer " + token,
+            },
+            });
+    setComments(response.data);
+}   catch (error){
+    console.log("fetchComments ", error.response.data)
+}
+}
+fetchComments();}, [token]);
+
+console.log(comments)
+return (
+    <div>
+    Users Comments
+    </div>
+)
+}
+
+export default GetComment;
