28f5781835c75859fb6fbee642063e46f0ef98ab
irishandyesiam <nathan.irish@gmail.com>
2023-06-21 15:27:22 -0500

    Removed unnessary console.log

--- frontend/src/components/Comments/Comments.jsx
+++ frontend/src/components/Comments/Comments.jsx
@@ -4,7 +4,6 @@ import {useEffect, useState} from 'react';
 import useAuth from '../../hooks/useAuth';
 
 const GetComment = (props) => {
-    console.log(props);
     const [user, token] = useAuth();
 
 
